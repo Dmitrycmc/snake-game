1ebb6bba3dd6a3d8f5f13c9af87d54570856eff4
Dmitry Lytov <dmitrycmc@gmail.com>
2019-10-14 05:57:22 +0300

    Updates

---
+++ precache-manifest.2eacabb2625e2b6b47491b1c13234ffc.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a755d0ffde03a58c68a",
+    "url": "/snake-game/static/js/runtime~main.356adb96.js"
+  },
+  {
+    "revision": "0e28b04ea188c8f44a69",
+    "url": "/snake-game/static/js/main.1800212f.chunk.js"
+  },
+  {
+    "revision": "a00e10a8b57e48165c32d1aa6969ffcd",
+    "url": "/snake-game/index.html"
+  }
+];
