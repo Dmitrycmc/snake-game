1ea90746427022069a2f63a39fa73c7f9d1c72ba
Dmitry Lytov <dmitrycmc@gmail.com>
2019-10-16 16:27:56 +0300

    Updates

---
+++ precache-manifest.89c9a6df9b433601b12c6e223d5726e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e7d342ac4ab9a969cd17529b6560b28",
+    "url": "/snake-game/static/media/pick.0e7d342a.wav"
+  },
+  {
+    "revision": "fe22c65dacfc11cd6818b3c9ac5e03d3",
+    "url": "/snake-game/static/media/hit.fe22c65d.wav"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/snake-game/static/media/digital-7 (mono).58045dab.ttf"
+  },
+  {
+    "revision": "9a755d0ffde03a58c68a",
+    "url": "/snake-game/static/js/runtime~main.356adb96.js"
+  },
+  {
+    "revision": "434ca7e7a35f2a7d9eec",
+    "url": "/snake-game/static/js/main.8ae82b82.chunk.js"
+  },
+  {
+    "revision": "434ca7e7a35f2a7d9eec",
+    "url": "/snake-game/static/css/main.aefb7221.chunk.css"
+  },
+  {
+    "revision": "0faed4308707bb46ded6555c43b5864c",
+    "url": "/snake-game/index.html"
+  }
+];
